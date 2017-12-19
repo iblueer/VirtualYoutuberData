4b9edf13e042b432cad50864c5bee9b9245fdd65
Maemolee <iblueer@live.cn>
2017-12-20 05:52:34 +0800

    Initial Commit

---
+++ VirtualYoutuberData/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  VirtualYoutuberData
+//
+//  Created by 宅音かがや on 2017/12/20.
+//  Copyright © 2017年 宅音かがや. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
